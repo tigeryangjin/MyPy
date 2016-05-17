0cbcd644a9028054ceb7ce289475962c8dfb065c
tigeryangjin <tigeryangjin@gmail.com>
2016-05-17 21:04:03 +0800

    backup

--- cx_Oracle.py
+++ cx_Oracle.py
@@ -1,8 +1,8 @@
 import cx_Oracle
   
-conn = cx_Oracle.connect('radm/RADM@ra-scan.bbgretek.com.cn/radb')
+conn = cx_Oracle.conn('radm/RADM@ra-scan.bbgretek.com.cn/radb')
 cursor = conn.cursor ()  
-cursor.execute ("select * from dual")  
+cursor.execute ('select sysdate from dual')
 row = cursor.fetchone ()  
 print(row[0])
   
