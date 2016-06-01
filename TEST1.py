d780128565f361e9bef5c7e566d6e700a9fcedc9
tigeryangjin <tigeryangjin@gmail.com>
2016-06-01 17:55:34 +0800

    backup

--- TEST1.py
+++ TEST1.py
@@ -1,12 +1,17 @@
 import cx_Oracle
 
-# conn = cx_Oracle.connect('rcx_Oracleadm/RADM@ra-scan.bbgretek.com.cn/radb')
-# conn = cx_Oracle.conn('rman/rman@192.168.2.210:1521/rman')
-conn = cx_Oracle.connect('rman', 'rman', '192.168.2.210:1521/rman')
-cursor = conn.cursor()
-cursor.execute("")
-row = cursor.fetchone()
-print(row[0])
+# sql执行语句变量
+sql_text = (
+    "select text from (select text,trunc(pm/10)+1 pm from (select 'copy from '||(case when instr(key_value_1,'13')=1 then 'bbgdq' when instr(key_value_1,'120072')=1 then 'bbgdq' when instr(key_value_1,'120040')=1 then 'bbgdq' else 'bbgcs' end)||'/drowssap@'||key_value_1||chr(32)||'to'||chr(32)||'bbgcs/drowssap@XL120017N159'||chr(32)||'INSERT'||chr(32)||'dpm001'||chr(32)||'using select * from dpm'||chr(59)||chr(10) text,a.key_value_1,store_name,a.state,description,(case when store_format in (1,2) then 1 else 2 end) store_format,row_number() over(partition by store_format order by a.state,key_value_1) pm from addr a,State b,store c where a.state=b.State and a.key_value_1=c.store and key_value_1 <'140000' and addr_type in (01) and module='ST'))")
 
+# 连接配置
+# conn = cx_Oracle.connect('rman', 'rman', '192.168.2.210:1521/rman')
+conn = cx_Oracle.connect('rms', 'Rms12345', 'dm03-scan.bbgretek.com.cn:1521/rmsdb')  # RMS
+# conn = cx_Oracle.connect('RADM', 'RADM', 'ra-scan.bbgretek.com.cn:1521/radb') #RADM
+cursor = conn.cursor()
+cursor.execute(sql_text)
+sql_result = cursor.fetchall()
 cursor.close()
 conn.close()
+
+# 保存结果到copy_ALL.sql
---
+++ Text_segmentation/bbg_store_tour.py
@@ -0,0 +1,19 @@
+import cx_Oracle
+
+# sql执行语句变量
+sql_text = (
+    "select text from (select text,trunc(pm/10)+1 pm from (select 'copy from '||(case when instr(key_value_1,'13')=1 then 'bbgdq' when instr(key_value_1,'120072')=1 then 'bbgdq' when instr(key_value_1,'120040')=1 then 'bbgdq' else 'bbgcs' end)||'/drowssap@'||key_value_1||chr(32)||'to'||chr(32)||'bbgcs/drowssap@XL120017N159'||chr(32)||'INSERT'||chr(32)||'dpm001'||chr(32)||'using select * from dpm'||chr(59)||chr(10) text,a.key_value_1,store_name,a.state,description,(case when store_format in (1,2) then 1 else 2 end) store_format,row_number() over(partition by store_format order by a.state,key_value_1) pm from addr a,State b,store c where a.state=b.State and a.key_value_1=c.store and key_value_1 <'140000' and addr_type in (01) and module='ST'))")
+
+# 连接配置
+# conn = cx_Oracle.connect('rman', 'rman', '192.168.2.210:1521/rman')
+conn = cx_Oracle.connect('rms', 'Rms12345', 'dm03-scan.bbgretek.com.cn:1521/rmsdb')  # RMS
+# conn = cx_Oracle.connect('RADM', 'RADM', 'ra-scan.bbgretek.com.cn:1521/radb') #RADM
+cursor = conn.cursor()
+cursor.execute(sql_text)
+sql_result = cursor.fetchall()
+cursor.close()
+conn.close()
+
+# 保存结果到copy_ALL.sql文件
+all_file = open('copy_ALL.sql', 'w')
+all_file.writelines(sql_result)
