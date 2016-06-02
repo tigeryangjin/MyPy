3f933c4982702e4e2d12ea7f90a59737dffca6b5
tigeryangjin <tigeryangjin@gmail.com>
2016-06-02 14:47:43 +0800

    backup

--- MyWork/yili_email.py
+++ MyWork/yili_email.py
@@ -3,12 +3,10 @@
 
 import cx_Oracle
 import os
-import csv
 import xlsxwriter
+import time
 
 os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'  # 设置中文编码
-
-# sys.setdefaultencoding('utf8')
 
 # Oracle查询SQL
 sql_file = open('D:\WORK\BBG\JOB\伊利\daily_report.sql', 'r')
@@ -25,17 +23,50 @@ sql_result = cursor.fetchall()
 cursor.close()
 conn.close()
 
-# 数据导出到excel文件
-path = 'D:\WORK\BBG\JOB\伊利\表格\ '
-path.strip()
-# excel_file = open(path + 'yj001.xls', 'w')
+# print(type(sql_result))
+# print(len(sql_result))
 # for i in range(len(sql_result)):
-#     excel_file.writelines(str(sql_result[i])+'\n')
-# excel_file.close()
+#     print(sql_result[i])
 
-with open(path + "XXX.xlsx", "w", newline="") as datacsv:
-    # dialect为打开csv文件的方式，默认是excel，delimiter="\t"参数指写入的时候的分隔符
-    csvwriter = csv.writer(datacsv, dialect=("excel"))
-    # csv文件插入一行数据，把下面列表中的每一项放入一个单元格（可以用循环插入多行）
-    for i in range(len(sql_result)):
-        csvwriter.writerow(str(sql_result[i])+'\n')
+# 生成excel文件-------------------------------------------------------
+# 生成excel文件名
+v_curr_time = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+v_file_name = 'py_' + v_curr_time + '.xlsx '
+print(v_file_name)
+workbook = xlsxwriter.Workbook(v_file_name)
+worksheet = workbook.add_worksheet()
+worksheet.write(0, 0, 'Hello Excel')
+worksheet.write(0, 0, '销售日期')
+worksheet.write(0, 1, '去年同期日期')
+worksheet.write(0, 2, '小类')
+worksheet.write(0, 3, '小类名称')
+worksheet.write(0, 4, '供应商编码')
+worksheet.write(0, 5, '供应商名称')
+worksheet.write(0, 6, '门店编码')
+worksheet.write(0, 7, '门店名称')
+worksheet.write(0, 8, '业态')
+worksheet.write(0, 9, '区域')
+worksheet.write(0, 10, '商品')
+worksheet.write(0, 11, '商品名称')
+worksheet.write(0, 12, '销售数量')
+worksheet.write(0, 13, '销售成本')
+worksheet.write(0, 14, '销售金额')
+worksheet.write(0, 15, '毛利额')
+worksheet.write(0, 16, '去年销售量')
+worksheet.write(0, 17, '去年销售成本')
+worksheet.write(0, 18, '去年销售金额')
+worksheet.write(0, 19, '去年毛利额')
+
+workbook.close()
+
+
+
+
+# 当查出多列数据时，需要一个cell一个cell的写入，要不然这四列就会写到excel的一个cell里
+# for i in range(len(sql_result)):
+#     for j in range(20):
+#         # print (rows[i][j])
+#         # print ("--------")
+#         sheet1.write(i + 1, j, sql_result[i][j])
+#
+# book.save(filename=v_file_name)
--- TEST1.py
+++ TEST1.py
@@ -1,2 +1,21 @@
-a='你好abc'
-print(a)
+from openpyxl import Workbook
+
+# 在内存中创建一个workbook对象，而且会至少创建一个 worksheet
+wb = Workbook()
+
+ws = wb.get_active_sheet()
+print(ws.title)
+
+ws.title = 'New Title'  # 设置worksheet的标题
+
+# 设置单元格的值
+ws.cell('D3').value = 4
+ws.cell(row=3, column=1).value = 6
+
+new_ws = wb.create_sheet(title='new_sheet')
+for row in range(100):
+    for col in range(10):
+        new_ws.cell(row=row, column=col).value = row + col
+
+# 最后一定要保存！
+wb.save(filename='new_file.xlsx')
