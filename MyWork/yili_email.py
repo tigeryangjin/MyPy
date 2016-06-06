41d6a15430e6009d8269b83be9cfdedd59befb51
tigeryangjin <tigeryangjin@gmail.com>
2016-06-06 19:39:43 +0800

    backup

--- MyWork/yili_email.py
+++ MyWork/yili_email.py
@@ -21,57 +21,67 @@ def export_excel_file(v_date):
         sql_text += sql_lines[i].strip() + ' '
 
     # 日期参数
-
+    v_year = int(v_date[0:4])
+    v_month = int(v_date[5:7])
+    v_day = int(v_date[8:10])
 
     # 配置信息
-    conn = cx_Oracle.connect('rms', 'Rms12345', 'dm03-scan.bbgretek.com.cn:1521/rmsdb')
-    cursor = conn.cursor()
-    cursor.execute(sql_text, {'TODAY': datetime.date(2016, 6, 5)})  # 传入日期参数
-    sql_result = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    try:
+        conn = cx_Oracle.connect('rms', 'Rms12345', 'dm03-scan.bbgretek.com.cn:1521/rmsdb')
+        cursor = conn.cursor()
+        cursor.execute(sql_text, {'TODAY': datetime.date(v_year, v_month, v_day)})  # 传入日期参数
+        sql_result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        print('SQL查询完成！')
+    except Exception as e:
+        print(Exception, ":", e)
 
     # 生成excel文件-------------------------------------------------------
     # 生成excel文件名
     path = 'D:\WORK\BBG\JOB\伊利\表格\\'
     v_file_name = path + 'YM_' + v_date + '.xlsx '
-    print(v_file_name)
+
     workbook = xlsxwriter.Workbook(v_file_name)
     # 创建
     worksheet = workbook.add_worksheet()
 
     # Add an Excel date format.
-    date_format = workbook.add_format({'num_format': 'yyyy/mm/dd'})
+    date_format = workbook.add_format({'num_format': 'yyyy/mm/dd', 'font_size': 9, 'font_name': '宋体'})
+    head_format = workbook.add_format({'bold': 1, 'font_size': 9, 'font_name': '宋体'})
+    data_format = workbook.add_format({'font_size': 9, 'font_name': '宋体'})
 
     # 写入列名
-    worksheet.write(0, 0, '销售日期')
-    worksheet.write(0, 1, '去年同期日期')
-    worksheet.write(0, 2, '小类')
-    worksheet.write(0, 3, '小类名称')
-    worksheet.write(0, 4, '供应商编码')
-    worksheet.write(0, 5, '供应商名称')
-    worksheet.write(0, 6, '门店编码')
-    worksheet.write(0, 7, '门店名称')
-    worksheet.write(0, 8, '业态')
-    worksheet.write(0, 9, '区域')
-    worksheet.write(0, 10, '商品')
-    worksheet.write(0, 11, '商品名称')
-    worksheet.write(0, 12, '销售数量')
-    worksheet.write(0, 13, '销售成本')
-    worksheet.write(0, 14, '销售金额')
-    worksheet.write(0, 15, '毛利额')
-    worksheet.write(0, 16, '去年销售量')
-    worksheet.write(0, 17, '去年销售成本')
-    worksheet.write(0, 18, '去年销售金额')
-    worksheet.write(0, 19, '去年毛利额')
+    worksheet.write(0, 0, '销售日期', head_format)
+    worksheet.write(0, 1, '去年同期日期', head_format)
+    worksheet.write(0, 2, '小类', head_format)
+    worksheet.write(0, 3, '小类名称', head_format)
+    worksheet.write(0, 4, '供应商编码', head_format)
+    worksheet.write(0, 5, '供应商名称', head_format)
+    worksheet.write(0, 6, '门店编码', head_format)
+    worksheet.write(0, 7, '门店名称', head_format)
+    worksheet.write(0, 8, '业态', head_format)
+    worksheet.write(0, 9, '区域', head_format)
+    worksheet.write(0, 10, '商品', head_format)
+    worksheet.write(0, 11, '商品名称', head_format)
+    worksheet.write(0, 12, '销售数量', head_format)
+    worksheet.write(0, 13, '销售成本', head_format)
+    worksheet.write(0, 14, '销售金额', head_format)
+    worksheet.write(0, 15, '毛利额', head_format)
+    worksheet.write(0, 16, '去年销售量', head_format)
+    worksheet.write(0, 17, '去年销售成本', head_format)
+    worksheet.write(0, 18, '去年销售金额', head_format)
+    worksheet.write(0, 19, '去年毛利额', head_format)
     for r in range(len(sql_result)):
         for c in range(len(sql_result[r])):
             if c in (0, 1):  # 第一、二列为日期格式
                 worksheet.write(r + 1, c, sql_result[r][c], date_format)
+
             else:
-                worksheet.write(r + 1, c, sql_result[r][c])
+                worksheet.write(r + 1, c, sql_result[r][c], data_format)
     workbook.close()
     print('导出到xlsx文件成功！')
+    print(v_file_name)
 
 
 # 发送邮件
@@ -104,7 +114,7 @@ def send_email(v_date):
         input()
     except Exception as e:
         print(Exception, ":", e)
-        input()
+        send_email(v_date)
 
 
 def yili_email(v_date):
@@ -114,4 +124,4 @@ def yili_email(v_date):
     send_email(v_date)
 
 
-export_excel_file('2016-06-01')
+yili_email(input('请输入日期（格式：yyyy-mm-dd）: '))
---
+++ TEST1.py
@@ -0,0 +1,7 @@
+def export_excel_file(v_date):
+    print(type(v_date))
+    print(int(v_date[0:4]))
+    print(int(v_date[5:7]))
+    print(int(v_date[8:10]))
+
+export_excel_file('2016-06-05')
