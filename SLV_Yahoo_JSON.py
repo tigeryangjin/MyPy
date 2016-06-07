1a4e63e4be7295c263f70ba83de1fbc926ae1e9b
tigeryangjin <tigeryangjin@gmail.com>
2016-06-07 14:59:43 +0800

    backup

--- SLV_Yahoo_JSON.py
+++ SLV_Yahoo_JSON.py
@@ -5,7 +5,7 @@ url = 'https://www.ishares.com/us/products/239855/ishares-silver-trust-fund'
 # 308774
 r = requests.get(url)
 data = r.text
-# i = data.index('10,491.18')
-i = 308774
+i = data.index('Tonnes in Trust')
+i += 215
 print('SLV持仓数量：', data[i: i + 9])
 input()
