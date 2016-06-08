415b8151e276481f411e4c93c7e4475d66c6ffac
tigeryangjin <tigeryangjin@gmail.com>
2016-06-08 08:40:19 +0800

    backup

--- TEST2.py
+++ TEST2.py
@@ -6,19 +6,21 @@ import matplotlib.pyplot as plt
 a = random.random(size=50)
 b = random.random(size=50)
 
-x = array(a)
-print(type(a))
-print(type(b))
-print(type(x))
-print(x[:, 1])
+x = array([a, b])
+
 # print(x)
 # basic
-# f1 = plt.figure(1)
-# plt.subplot(211)
-# plt.scatter(x[:, 1], x[:, 0])
-#
-# # with label
-# plt.subplot(212)
-# label = list(ones(20)) + list(2 * ones(15)) + list(3 * ones(15))
-# label = array(label)
+f1 = plt.figure(1)
+plt.subplot(211)
+plt.scatter(x[:, 1], x[:, 0])
+
+# with label
+plt.subplot(212)
+label = list(ones(20)) + list(2 * ones(15)) + list(3 * ones(15))
+label = array(label)
+
 # plt.scatter(x[:, 1], x[:, 0], 15.0 * label, 15.0 * label)
+print(x)
+print(x[:, 0])
+print(x[:, 1])
+print(label)
--- TEST3.py
+++ TEST3.py
@@ -1,3 +1,4 @@
-import random
-
-x = random.randrange(50,30)
+from numpy import *
+c = array([[1, 2, 3, 4],[4, 5, 6, 7], [7, 8, 9, 10]])
+print(type(c))
+print(c)
