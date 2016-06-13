d571e0023cece1fbde2f2958fd2489a0195b7624
tigeryangjin <tigeryangjin@gmail.com>
2016-06-13 20:56:33 +0800

    backup

--- TEST4.py
+++ TEST4.py
@@ -1,10 +1,50 @@
-import datetime
+import smtplib
+import email.mime.multipart
+import email.mime.text
+import time
+import schedule
 
 
-def get_v_date(default=str(datetime.date.today() - datetime.timedelta(days=1))):
-    r = input('请输入日期（格式：yyyy-mm-dd，默认值为昨天）: ')
-    if r == '':
-        return default
-    return r
+def send_email():
+    # 构造邮件头和正文
+    msg = email.mime.multipart.MIMEMultipart()
+    msg['from'] = 'tigeryangjin@gmail.com'
+    msg['to'] = '1370365906@qq.com'
+    msg['subject'] = 'hello,yangjin'+str(time.time())
+    content = '''发送自Python '''  # 邮件正文
+    txt = email.mime.text.MIMEText(content)
+    msg.attach(txt)
 
-print(get_v_date())
+    # 构造附件
+    # attachment_name = v_file_name  # 附件名称
+    # att1 = email.mime.text.MIMEText(open('D:\WORK\BBG\JOB\伊利\表格\\' + attachment_name, 'rb').read(), 'base64', 'utf-8')
+    # att1["Content-Type"] = 'application/octet-stream'
+    # att1["Content-Disposition"] = 'attachment; filename=' + attachment_name  # 邮件显示的附件名称
+    # msg.attach(att1)
+
+    # 发送邮件
+    try:
+        smtp = smtplib.SMTP()
+        smtp.connect('smtp-mail.outlook.com', '25')  # 连接到发邮件服务器 端口：25、587
+        smtp.starttls()  # 开启TLS/SSL加密
+        smtp.login('tigeryangjin@outlook.com', 'tiger19790909')  # 登录邮箱
+        smtp.sendmail('tigeryangjin@outlook.com', '12109471@qq.com', str(msg))  # 发送邮件
+        smtp.quit()
+        print('邮件发送成功！')
+    except Exception as e:
+        print(Exception, ":", e)
+        send_email()
+
+
+schedule.every(1).minutes.do(send_email)
+
+
+# schedule.every(1).minutes.do(job)
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- mydef.py
+++ mydef.py
@@ -13,8 +13,6 @@ def zfb(min_person, max_person, total_amt):
     plt.show()
 
 
-# zfb(1000, 3000, 20000)
-
 def writer_to_csv():
     import csv
 
@@ -82,11 +80,6 @@ class StudentsScore:
             return "D"
         else:
             return "F"
-
-
-lloyd = StudentsScore('lloyd', [90.0, 97.0, 75.0, 92.0], [88.0, 40.0, 94.0], [75.0, 90.0])
-alice = StudentsScore('alice', [100.0, 92.0, 98.0, 100.0], [82.0, 83.0, 91.0], [89.0, 97.0])
-tyler = StudentsScore('Tyler', [0.0, 87.0, 75.0, 22.0], [0.0, 75.0, 78.0], [100.0, 100.0])
 
 
 # 斐波那契数列 方法一
@@ -164,4 +157,52 @@ def matrix_mcl():
     print(c)
 
 
-matrix_mcl()
+def schedule_email():
+    # 定时发送邮件
+    import smtplib
+    import email.mime.multipart
+    import email.mime.text
+    import time
+    import schedule
+
+    def send_email():
+        # 构造邮件头和正文
+        msg = email.mime.multipart.MIMEMultipart()
+        msg['from'] = 'tigeryangjin@gmail.com'
+        msg['to'] = '1370365906@qq.com'
+        msg['subject'] = 'hello,yangjin' + str(time.time())
+        content = '''发送自Python '''  # 邮件正文
+        txt = email.mime.text.MIMEText(content)
+        msg.attach(txt)
+
+        # 构造附件
+        # attachment_name = v_file_name  # 附件名称
+        # att1 = email.mime.text.MIMEText(open('D:\WORK\BBG\JOB\伊利\表格\\' + attachment_name, 'rb').read(), 'base64', 'utf-8')
+        # att1["Content-Type"] = 'application/octet-stream'
+        # att1["Content-Disposition"] = 'attachment; filename=' + attachment_name  # 邮件显示的附件名称
+        # msg.attach(att1)
+
+        # 发送邮件
+        try:
+            smtp = smtplib.SMTP()
+            smtp.connect('smtp-mail.outlook.com', '25')  # 连接到发邮件服务器 端口：25、587
+            smtp.starttls()  # 开启TLS/SSL加密
+            smtp.login('tigeryangjin@outlook.com', 'tiger19790909')  # 登录邮箱
+            smtp.sendmail('tigeryangjin@outlook.com', '12109471@qq.com', str(msg))  # 发送邮件
+            smtp.quit()
+            print('邮件发送成功！')
+        except Exception as e:
+            print(Exception, ":", e)
+            send_email()
+
+    schedule.every(1).minutes.do(send_email)
+
+    # schedule.every(1).minutes.do(job)
+    # schedule.every().hour.do(job)
+    # schedule.every().day.at("10:30").do(job)
+    # schedule.every().monday.do(job)
+    # schedule.every().wednesday.at("13:15").do(job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
