c5635b67f25882a5485cc2d9510ab76e494e890d
tigeryangjin <tigeryangjin@gmail.com>
2016-06-13 11:34:03 +0800

    backup

--- TEST4.py
+++ TEST4.py
@@ -1,13 +1,13 @@
 import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
 
-a = [[20, 5], [15, 10]]
-a = np.asmatrix(a)
-b = [[2, 1], [1, 4]]
-b = np.mat(b)
-print("matrix a:")
-print(a)
-print("matrix b:")
-print(b)
-c = a * b
-print("matrix c:")
-print(c)
+pd.set_option('display.width', 200)
+
+dates = pd.date_range('20150101', periods=5)
+df = pd.DataFrame(np.random.randn(5, 4), index=dates, columns=list('ABCD'))
+print(df)
+
+stock_list = ['000001.XSHE', '000002.XSHE', '000568.XSHE', '000625.XSHE', '000768.XSHE', '600028.XSHG', '600030.XSHG', '601111.XSHG', '601390.XSHG', '601998.XSHG']
+raw_data = DataAPI.MktEqudGet(secID=stock_list, beginDate='20150101', endDate='20150131', pandas='1')
+df = raw_data[['secID', 'tradeDate', 'secShortName', 'openPrice', 'highestPrice', 'lowestPrice', 'closePrice', 'turnoverVol']]
