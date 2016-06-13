0fd30820c8a1bcddd2955d7e4790921035d8c45d
tigeryangjin <tigeryangjin@gmail.com>
2016-06-13 15:55:55 +0800

    backup

--- MyWork/yili_email_test.py
+++ MyWork/yili_email_test.py
@@ -118,29 +118,84 @@ def send_email_daily(v_date):
         send_email_daily(v_date)
 
 
-# 默认日期为昨日
+def dq_yili_215(v_bdate, v_edate):
+    # 伊利215中类档期数据
+    # Oracle查询SQL
+    sql_file = open('D:\WORK\BBG\JOB\伊利\DQ_yili_215.sql', 'r')
+    sql_lines = sql_file.readlines()
+    sql_text = ''
+    for i in range(len(sql_lines)):
+        sql_text += sql_lines[i].strip() + ' '
+
+    # 日期参数
+    v_byear = int(v_bdate[0:4])
+    v_bmonth = int(v_bdate[5:7])
+    v_bday = int(v_bdate[8:10])
+    v_eyear = int(v_edate[0:4])
+    v_emonth = int(v_edate[5:7])
+    v_eday = int(v_edate[8:10])
+
+    # 配置信息
+    try:
+        conn = cx_Oracle.connect('rms', 'Rms12345', 'dm03-scan.bbgretek.com.cn:1521/rmsdb')
+        cursor = conn.cursor()
+        cursor.execute(sql_text, {'BDATE': datetime.date(v_byear, v_bmonth, v_bday),
+                                  'EDATE': datetime.date(v_eyear, v_emonth, v_eday)})  # 传入日期参数
+        sql_result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        print('SQL查询完成！')
+    except Exception as e:
+        print(Exception, ":", e)
+
+        # 生成excel文件-------------------------------------------------------
+        # 生成excel文件名
+        path = 'D:\WORK\BBG\JOB\伊利\表格\\'
+        v_file_name = path + 'DQ_' + v_bdate + '~' + v_edate + '.xlsx '
+
+        workbook = xlsxwriter.Workbook(v_file_name)
+        # 创建
+        worksheet = workbook.add_worksheet()
+
+        # Add an Excel date format.
+        # date_format = workbook.add_format({'num_format': 'yyyy/mm/dd', 'font_size': 9, 'font_name': '宋体'})
+        head_format = workbook.add_format({'bold': 1, 'font_size': 9, 'font_name': '宋体'})
+        data_format = workbook.add_format({'font_size': 9, 'font_name': '宋体'})
+
+        # 写入列名
+        worksheet.write(0, 0, '门店编码', head_format)
+        worksheet.write(0, 1, '门店名称', head_format)
+        worksheet.write(0, 2, '商品', head_format)
+        worksheet.write(0, 3, '商品名称', head_format)
+        worksheet.write(0, 4, '供应商编码', head_format)
+        worksheet.write(0, 5, '供应商名称', head_format)
+        worksheet.write(0, 6, '销售数量', head_format)
+        worksheet.write(0, 7, '销售成本', head_format)
+        worksheet.write(0, 8, '销售金额', head_format)
+        worksheet.write(0, 9, '毛利额', head_format)
+        for r in range(len(sql_result)):
+            for c in range(len(sql_result[r])):
+                worksheet.write(r + 1, c, sql_result[r][c], data_format)
+        workbook.close()
+        print('导出到xlsx文件成功！')
+        print(v_file_name)
+
+
 def yili_email_daily(default=str(datetime.date.today() - datetime.timedelta(days=1))):
-    v_date = input('请输入日期（格式：yyyy-mm-dd，默认值为昨天）: ')
+    # 每日销售数据
+    v_date = input('请输入日期（格式：yyyy-mm-dd，默认值为昨天）:\n ')
+    # 导出xlsx文件到磁盘
+    # export_excel_file_daily(v_date)
+    # 导出文件作为附件发送邮件
+    # send_email_daily(v_date)
     if v_date == '':
         # 如果v_date为空值则取default
         export_excel_file_daily(default)
-        # send_email_daily(default)
+        send_email_daily(default)
     else:
         # 如果v_date不为空值则取v_date
         export_excel_file_daily(v_date)
-        # send_email_daily(v_date)
-
-
-# def get_v_date(default=str(datetime.date.today() - datetime.timedelta(days=1))):
-#     r = input('请输入日期（格式：yyyy-mm-dd，默认值为昨天）: ')
-#     if r == '':
-#         return default
-#     return r
+        send_email_daily(v_date)
 
 
 yili_email_daily()
-
-# today = datetime.date.today()
-# yesterday = str(today - datetime.timedelta(days=1))
-# print(yesterday)
-# print(type(yesterday))
