2ac7b8b732438a827654154621392d1f81b38727
tigeryangjin <tigeryangjin@gmail.com>
2016-06-22 12:29:45 +0800

    backup

--- 163_com_music_playlist_random.py
+++ 163_com_music_playlist_random.py
@@ -1,6 +1,6 @@
-#网易云音乐歌单随机浏览
-#By YangJin
-#Python3.5
+# 网易云音乐歌单随机浏览
+# By YangJin
+# Python3.5
 
 import requests
 import urllib
@@ -9,13 +9,13 @@ import os
 import random
 
 for i in range(10):
-    id=random.randint(1,99999999)
-    url='http://music.163.com/api/playlist/detail?id='+str(id)
+    id = random.randint(1, 99999999)
+    url = 'http://music.163.com/api/playlist/detail?id=' + str(id)
     r = requests.get(url)
     status_code = r.json()['code']
     if status_code == 200:
         playlist_name = r.json()['result']['name']
-        tractCount=r.json()['result']['tractCount']
-        print(str(id)+'|'+str(playlist_name)+str(tractCount))
+        t = r.json()['result']['tracks']
+        print(str(id) + '\n' + str(playlist_name) + '\n' + str(t))
     else:
         pass
