18ba7e9658da8cb9893377c698f1565a7aeaa509
tigeryangjin <tigeryangjin@gmail.com>
2016-07-19 16:48:14 +0800

    backup

--- MaxPrime.py
+++ MaxPrime.py
@@ -16,13 +16,14 @@ def is_prime(x):
         return True
 
 
-def prime(x):
+def prime_list(x):
     # 返回素数列表prime_list
     global PRIME_LIST
+    PRIME_LIST = []
     num = 1
     while num >= 1:
         if is_prime(num) is True:
-            PRIME_LIST.append(num)
+            PRIME_LIST.append(int(num))
         num += 2
         if num >= math.sqrt(x):
             break
@@ -31,13 +32,15 @@ def prime(x):
 
 def prime_factorization(m):
     # 整数(m)分解成二个素数（p,q）的乘积
-    # 988027
-    prime(int(math.sqrt(m)))
+    # 991*997=988027
+    prime_list(int(m))
     for i in range(len(PRIME_LIST)):
-        if m / PRIME_LIST[i] == int(m / PRIME_LIST[i]):
-            return m, m / PRIME_LIST
+        p = PRIME_LIST[i]
+        q = m / p
+        if q == int(q):
+            return p, int(q)
+            # return m, int(m / PRIME_LIST)
 
 
-print(prime_factorization(988027))
-print(PRIME_LIST)
-print(int(math.sqrt(988027)))
+print(prime_factorization(1427247692705959880439315947500961989719490561))
+
--- TEST1.py
+++ TEST1.py
@@ -1,6 +1,7 @@
-import math
-
-m = 988027
-n = math.sqrt(m)
-print(n)
-print(int(n))
+m = 10000000
+for p in range(m):
+    for q in range(m):
+        if 2 * p ** 2 == q ** 2:
+            print(p, q)
+        else:
+            pass
