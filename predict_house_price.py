b3e1d6e34ec7a659c8e3522d8df3a8b42c7663c6
tigeryangjin <tigeryangjin@gmail.com>
2016-11-03 17:10:39 +0800

    backup

--- predict_house_price.py
+++ predict_house_price.py
@@ -29,9 +29,23 @@ def linear_model_main(X_parameters, Y_parameters, predict_value):
     return predictions
 
 
+# Function to show the resutls of linear fit model
+def show_linear_line(X_parameters, Y_parameters):
+    # Create linear regression object
+    regr = linear_model.LinearRegression()
+    regr.fit(X_parameters, Y_parameters)
+    plt.scatter(X_parameters, Y_parameters, color='blue')
+    plt.plot(X_parameters, regr.predict(X_parameters), color='red', linewidth=4)
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
+
+
 X, Y = get_data('F:\Documents\MyPy\MyPy\input_data.csv')
 predictvalue = 700
 result = linear_model_main(X, Y, predictvalue)
 print("Intercept value ", result['intercept'])
 print("coefficient", result['coefficient'])
 print("Predicted value: ", result['predicted_value'])
+
+show_linear_line(X, Y)
