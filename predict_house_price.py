2b093c3d7f9040b376883ade4497fb57d5d6b407
tigeryangjin <tigeryangjin@gmail.com>
2016-11-03 17:08:22 +0800

    backup

--- predict_house_price.py
+++ predict_house_price.py
@@ -16,4 +16,22 @@ def get_data(file_name):
     return X_parameter, Y_parameter
 
 
-print(get_data('F:\Documents\MyPy\MyPy\input_data.csv'))
+# Function for Fitting our data to Linear model
+def linear_model_main(X_parameters, Y_parameters, predict_value):
+    # Create linear regression object
+    regr = linear_model.LinearRegression()
+    regr.fit(X_parameters, Y_parameters)
+    predict_outcome = regr.predict(predict_value)
+    predictions = {}
+    predictions['intercept'] = regr.intercept_
+    predictions['coefficient'] = regr.coef_
+    predictions['predicted_value'] = predict_outcome
+    return predictions
+
+
+X, Y = get_data('F:\Documents\MyPy\MyPy\input_data.csv')
+predictvalue = 700
+result = linear_model_main(X, Y, predictvalue)
+print("Intercept value ", result['intercept'])
+print("coefficient", result['coefficient'])
+print("Predicted value: ", result['predicted_value'])
