189da7477733e7047c0a356bde290d5eebc896e8
tigeryangjin <tigeryangjin@gmail.com>
2016-11-15 14:17:41 +0800

    backup

--- TEST3.py
+++ TEST3.py
@@ -1,55 +1,19 @@
-# Required Packages
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from sklearn import datasets, linear_model
+import random
+
+a = []
+a.append(random.randint)
+a.append(random.randint)
+a.append(random.randint)
+a.append(random.randint)
+print(a)
+print(random.randint)
+
+def px(a, b):
+    if a < b:
+        a, b = b, a
+    else:
+        a, b = a, b
+    return a, b
 
 
-# Function to get data
-def get_data(file_name):
-    data = pd.read_csv(file_name)
-    X_parameter = []
-    Y_parameter = []
-    for single_square_feet, single_price_value in zip(data['year'], data['sales']):
-        X_parameter.append([float(single_square_feet)])
-        Y_parameter.append(float(single_price_value))
-    return X_parameter, Y_parameter
-
-
-# Function for Fitting our data to Linear model
-def linear_model_main(X_parameters, Y_parameters, predict_value):
-    # Create linear regression object
-    regr = linear_model.BayesianRidge()
-    regr.fit(X_parameters, Y_parameters)
-    predict_outcome = regr.predict(predict_value)
-    predictions = {}
-
-    predictions['intercept'] = regr.intercept_
-    predictions['coefficient'] = regr.coef_
-    predictions['predicted_value'] = predict_outcome
-    predictions['score'] = regr.score(X_parameters, Y_parameters)
-    return predictions
-
-
-# Function to show the resutls of linear fit model
-def show_linear_line(X_parameters, Y_parameters):
-    # Create linear regression object
-    regr = linear_model.LinearRegression()
-    regr.fit(X_parameters, Y_parameters)
-    plt.scatter(X_parameters, Y_parameters, color='blue')
-    plt.plot(X_parameters, regr.predict(X_parameters), color='red', linewidth=4)
-    plt.xticks(())
-    plt.yticks(())
-    plt.show()
-
-
-X, Y = get_data('F:\Documents\MyPy\MyPy\input_data.csv')
-predictvalue = 15
-result = linear_model_main(X, Y, predictvalue)
-print("Intercept value ", result['intercept'])
-print("coefficient", result['coefficient'])
-print("Predicted value: ", result['predicted_value'])
-print("Score: ", result['score'])
-print('Y=', result['intercept'], '+(', result['coefficient'][0], '*X)')
-
-show_linear_line(X, Y)
+print(px(3, 5))
