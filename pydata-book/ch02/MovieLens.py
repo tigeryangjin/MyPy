0fcf887adedb7265bb1136daa38d6ee84fc7be3e
tigeryangjin <tigeryangjin@gmail.com>
2017-02-14 09:26:22 +0800

    commit

--- pydata-book/ch02/MovieLens.py
+++ pydata-book/ch02/MovieLens.py
@@ -1,6 +1,21 @@
 import pandas as pd
 
 unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
-users = pd.read_table('users.dat', sep='::', header=None, names=unames)
-rnames=['user_id','movie_id','rating','timestamp']
-ratings=pd.read_table('')
+users = pd.read_table('ml-1m/users.dat', sep='::', header=None, names=unames, engine='python')
+
+rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
+ratings = pd.read_table('ml-1m/ratings.dat', sep='::', header=None, names=rnames, engine='python')
+
+mnames = ['movie_id', 'title', 'genres']
+movies = pd.read_table('ml-1m/movies.dat', sep='::', header=None, names=mnames, engine='python')
+
+data = pd.merge(pd.merge(ratings, users), movies)
+
+mean_ratings = data.pivot_table('rating', index='title', columns='gender', aggfunc='mean')
+ratings_by_title = data.groupby('title').size()
+ratings_by_gender = data.groupby('gender').size()
+active_titles = ratings_by_title.index[ratings_by_title >= 2000]
+mean_ratings = mean_ratings.ix[active_titles]
+top_female_ratings = mean_ratings.sort_index(by='F', ascending=False)
+print(mean_ratings)
+print(type(mean_ratings))
---
+++ pydata-book/ch02/names.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+names1880 = pd.read_csv('names/yob1880.txt', names=['name', 'sex', 'births'])
