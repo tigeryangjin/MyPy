7115e89ae7c3b6d0b19f6abb038f62f780ceabab
tigeryangjin <tigeryangjin@gmail.com>
2017-03-29 18:58:09 +0800

    commit

--- TEST1.py
+++ TEST1.py
@@ -1,5 +1,9 @@
+import requests
 import re
 
-nojoke=re.findall(r'[ab]c','aaaccbbbccc123')
-print(nojoke)
-
+url = 'http://live.sina.com.cn/zt/app_zt/f/v/finance/globalnews1/?page=1'
+html_source = requests.get(url).text
+time = re.findall(r'<p class="bd_i_time_c">(.*?)</p>', html_source)
+messages = re.findall(r'<p class="bd_i_txt_c">(.*?)</p>', html_source)
+for i in range(len(messages)):
+    print(time[i],messages[i])
--- TEST2.py
+++ TEST2.py
@@ -1,11 +1,27 @@
-import numpy as np
-import pandas as pd
 
-data1 = pd.DataFrame({'item': ['a', 'b', 'c', 'd'], 'in_qty': [1, 3, 5, 7]})
-data2 = pd.DataFrame({'item': ['a', 'b', 'c', 'e'], 'out_qty': [2, 3, 6, 10]})
 
-print(data1)
-print(data2)
-# print(pd.merge(data1,data2,how='inner'))
-print(pd.merge(data1,data2))
-print(data1.combine_first(data2))
+import requests
+import json
+
+
+def test():
+    url = "https://www.zhihu.com/topic/19550994/hot"
+    headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
+        'Connection': 'keep-alive',
+        'Content-Length': '27',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': '这里的内容弄你自己的吧，我就不把我的账户cookie信息放上来了',
+        'Host': 'www.zhihu.com',
+        'Origin': 'https://www.zhihu.com',
+        'Referer': 'https://www.zhihu.com/topic/19550994/hot',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-Xsrftoken': 'f56ce1182d8e4ada986e83150d131a16'}
+
+    re = requests.post(url, headers=headers, data={'start': 0, 'offset': 3047.3986818})
+    return json.loads(re.text)
+
+print(test())
