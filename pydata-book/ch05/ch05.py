f5f9b2d25cf100e5b67632744880fcbf8ecc6de4
tiger <tigeryangjin@outlook.com>
2017-07-22 10:32:16 +0800

    backup

--- pydata-book/ch05/ch05.py
+++ pydata-book/ch05/ch05.py
@@ -1,6 +1,7 @@
 from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
+import pandas.io.data as web
 
 obj = Series([4, 7, -5, 3])
 obj2 = Series([4, 7, -5, 3], index=['d', 'b', 'a', 'c'])
@@ -10,3 +11,11 @@ data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'],
         'year': [2000, 2001, 2002, 2001, 2002],
         'pop': [1.5, 1.7, 3.6, 2.4, 2.9]}
 frame2 = DataFrame(data, columns=['year', 'state', 'pop', 'debt'], index=['one', 'two', 'three', 'four', 'five'])
+pop = {'Nevada': {2001: 2.4, 2002: 2.9}, 'Ohio': {2001: 1.5, 2002: 3.6, 2003: 1.8}}
+obj = Series([4.5, 7.2, -5.3, 3.6], index=['d', 'b', 'a', 'c'])
+
+all_data = {}
+for ticker in ['AAPL', 'IBM', 'MSFT', 'GOOG']:
+    all_data[ticker] = web.get_data_yahoo(ticker, '1/1/2000', '1/1/2010')
+price = DataFrame({tic: data['Adj Close'] for tic, data in all_data.items()})
+volume = DataFrame({tic: data['Volume'] for tic, data in all_data.items()})
