5900a7565bf6b436d7f9525ef83994d4f8545cd2
tiger <tigeryangjin@outlook.com>
2017-09-03 22:37:39 +0800

    backup

--- Machine Learning in Action/apriori.py
+++ Machine Learning in Action/apriori.py
@@ -1,15 +1,41 @@
 def loadDataSet():
     return [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
 
+
 def createC1(dataSet):
-    C1=[]
+    C1 = []
     for transaction in dataSet:
         for item in transaction:
             if not [item] in C1:
-                C1. append([ item])
-    C1. sort() #❶ 对 C1 中 每个 项 构建 一个 不变 集合
-    return map( frozenset, C1)
-
-print(loadDataSet())
+                C1.append([item])
+    C1.sort()  # 对C1中每个项构建一个不变集合
+    return map(frozenset, C1)
 
 
+def scanD(D, Ck, minSupport):
+    ssCnt = {}
+    for tid in D:
+        for can in Ck:
+            if can.issubset(tid):
+                if not ssCnt.has_key(can):
+                    ssCnt[can] = 1
+                else:
+                    ssCnt[can] += 1
+    numItems = float(len(list(D)))
+    retList = []
+    supportData = {}
+    for key in ssCnt:
+        # 计算所有项集的支持度
+        support = ssCnt[key] / numItems
+        if support >= minSupport: retList.insert(0, key)
+        supportData[key] = support
+    return retList, supportData
+
+
+dataSet = loadDataSet()
+C1 = createC1(dataSet)
+print(list(C1))
+D = map(set, dataSet)
+print(list(D))
+L1, suppData0 = scanD(D, C1, 0.5)
+print(L1)
--- TEST1.py
+++ TEST1.py
@@ -1,7 +1,2 @@
-from pandas import Series
-
-obj = Series([4, 7, -5, 3])
-obj2 = Series([4, 7, -5, 3], index=['d', 'b', 'a', 'c'])
-sdata = {'Ohio': 35000, 'Texas': 71000, 'Oregon': 16000, 'Utah': 5000}
-states = ['California', 'Ohio', 'Oregon', 'Texas']
-obj3 = Series(sdata)
+set1=frozenset([1,2,3,4])
+print(set1)
