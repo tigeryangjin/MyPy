b4489af400d7352ef9cf770d14cbe82a7e040f46
tiger <tigeryangjin@outlook.com>
2017-09-18 20:55:16 +0800

    backup

--- pydata-book/ch06/ch06.py
+++ pydata-book/ch06/ch06.py
@@ -1,4 +1,10 @@
-import pandas as pd
-import sys
-data=pd.read_csv('ex5.csv')
-data.to_csv(sys.stdout,sep='|',na_rep='NULL')
+from lxml.html import parse
+from urllib.request import urlopen
+
+request = urlopen('http://finance.yahoo.com/q/op?s=APPL+Options')
+parsed = parse(request)
+# parsed = parse(urlopen('http://finance.yahoo.com/q/op?s=APPL+Options'))
+doc = parsed.getroot()
+urls = [lnk.get('href') for lnk in doc.findall('.//a')]
+print(urls)
+request.close()
