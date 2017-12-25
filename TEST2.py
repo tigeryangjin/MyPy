45b1aba5784e78242870506b642b11a50654116b
tigeryangjin <tigeryangjin@gmail.com>
2017-12-25 21:07:30 +0800

    backup

--- TEST2.py
+++ TEST2.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 
-matrix1=tf.constant([[3.,3.]])
-matrix2=tf.constant([[2.],[2.]])
-product=tf.matmul(matrix1,matrix2)
+matrix1 = tf.constant([[3., 3.]])
+matrix2 = tf.constant([[2.], [2.]])
+product = tf.matmul(matrix1, matrix2)
 
-sess=tf.session()
-result=sess.run(product)
-print(result)
+with tf.Session() as sess:
+    result = sess.run([product])
+    print(result)
