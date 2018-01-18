bff7ae68c29cbb66dd96cc66cadd71622c1de0f1
tigeryangjin <tigeryangjin@gmail.com>
2018-01-18 21:46:22 +0800

    backup

--- TEST2.py
+++ TEST2.py
@@ -1,9 +1,3 @@
-import tensorflow as tf
+import hello
 
-matrix1 = tf.constant([[3., 3.]])
-matrix2 = tf.constant([[2.], [2.]])
-product = tf.matmul(matrix1, matrix2)
-
-with tf.Session() as sess:
-    result = sess.run([product])
-    print(result)
+hello.hi()
---
+++ hello/__init__.py
@@ -0,0 +1,5 @@
+# hello 模块的 __init__.py
+# -*- coding: utf-8 -*-
+
+def hi():
+    print("hello,my name is yangjin")
