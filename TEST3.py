4301680a9c2736a6582cf2b1c851b1d203b2e4db
tigeryangjin <tigeryangjin@gmail.com>
2018-05-08 18:56:16 +0800

    backup

--- TEST3.py
+++ TEST3.py
@@ -1,27 +1,9 @@
-from numpy import *
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-trainMat = [[1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
-            [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0],
-            [0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-            [0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]]
-listClasses = [0, 1, 0, 1, 0, 1]
-numWords = len(trainMat[0])
-p0Num = zeros(numWords)
-p1Num = zeros(numWords)
-p0Denom = 0.0
-p1Denom = 0.0
-for i in range(len(trainMat)):
-    if listClasses[i] == 1:
-        p1Num += trainMat[i]
-        p1Denom += sum(trainMat[i])
-    else:
-        p0Num += trainMat[i]
-        p0Denom += sum(trainMat[i])
-print('p0Num: ', p0Num)
-print('p0Denom: ', p0Denom)
-print('p0Num / p0Denom: ', p0Num / p0Denom)
-print('p1Num: ', p1Num)
-print('p1Denom: ', p1Denom)
-print('p1Num / p1Denom: ', p1Num / p1Denom)
+html = urlopen('http://jr.jd.com')
+bs_obj = BeautifulSoup(html.read(), 'html.parser')
+text_list = bs_obj.find_all("a", "nav-item-primary")
+for text in text_list:
+    print(text.get_text())
+html.close()
